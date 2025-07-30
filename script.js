43a8d44b7c8183fa72a444068892de3a9168ccfd
NguyenVanHai77 <Nguyenvanhai38679@gmail.com>
2025-07-30 11:24:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
-
 const pianoKeys = document.querySelectorAll(".piano-keys .key"),
-volumeSlider = document.querySelector(".volume-slider input"),
-keysCheckbox = document.querySelector(".keys-checkbox input");
+      volumeSlider = document.querySelector(".volume-slider input"),
+      keysCheckbox = document.querySelector(".keys-checkbox input");
 
-let allKeys = [],
-audio = new Audio(`tunes/a.wav`); // by default, audio src is "a" tune
+let allKeys = [];
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 const playTune = (key) => {
-    audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed 
-    audio.play(); // playing audio
+    // Ánh xạ phím với tần số (Hz) của nốt nhạc
+    const frequencies = {
+        'a': 261.63, // C4
+        'w': 277.18, // C#4
+        's': 293.66, // D4
+        'e': 311.13, // D#4
+        'd': 329.63, // E4
+        'f': 349.23, // F4
+        't': 369.99, // F#4
+        'g': 392.00, // G4
+        'y': 415.30, // G#4
+        'h': 440.00, // A4
+        'u': 466.16, // A#4
+        'j': 493.88, // B4
+        'k': 523.25, // C5
+        'o': 554.37, // C#5
+        'l': 587.33, // D5
+        'p': 622.25, // D#5
+        ';': 659.25  // E5
+    };
 
-    const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
-    clickedKey.classList.add("active"); // adding active class to the clicked key element
-    setTimeout(() => { // removing active class after 150 ms from the clicked key element
-        clickedKey.classList.remove("active");
-    }, 150);
-}
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+    oscillator.type = 'sine'; // Sử dụng sóng sine để mô phỏng âm piano đơn giản
+    oscillator.frequency.setValueAtTime(frequencies[key] || 440, audioContext.currentTime);
+    gainNode.gain.setValueAtTime(volumeSlider.value, audioContext.currentTime);
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    oscillator.start();
+    gainNode.gain.exponentialRampToValueAtTime(0.0001, audioContext.currentTime + 1);
+    oscillator.stop(audioContext.currentTime + 1);
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    if (clickedKey) {
+        clickedKey.classList.add("active");
+        setTimeout(() => {
+            clickedKey.classList.remove("active");
+        }, 150);
+    }
+};
 
 pianoKeys.forEach(key => {
-    allKeys.push(key.dataset.key); // adding data-key value to the allKeys array
-    // calling playTune function with passing data-key value as an argument
+    allKeys.push(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
 const handleVolume = (e) => {
-    audio.volume = e.target.value; // passing the range slider value as an audio volume
-}
+    // Không cần xử lý thêm vì volume đã được áp dụng trong playTune
+};
 
 const showHideKeys = () => {
-    // toggling hide class from each key on the checkbox click
     pianoKeys.forEach(key => key.classList.toggle("hide"));
-}
+};
 
 const pressedKey = (e) => {
-    // if the pressed key is in the allKeys array, only call the playTune function
-    if(allKeys.includes(e.key)) playTune(e.key);
-}
+    if (allKeys.includes(e.key)) playTune(e.key);
+};
 
 keysCheckbox.addEventListener("click", showHideKeys);
 volumeSlider.addEventListener("input", handleVolume);
